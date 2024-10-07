7aaae1c449f06a13d2809248a997ef3ddc9e332a
Sajib Hasan <sajib.thinkcrypt@gmail.com>
2024-10-07 10:27:56 +0600

    base url dynamic

---
+++ src/app/petromax/success/page.tsx
@@ -0,0 +1,14 @@
+import { Box, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Box w="full" h="100vh">
+        <Flex>
+            
+        </Flex>
+    </Box>
+  )
+}
+
+export default page
