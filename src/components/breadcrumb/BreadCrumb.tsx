cf4d783daeb3adb2fd8b8a316b7ae955a165d68a
Sajib Hasan <sajib.thinkcrypt@gmail.com>
2024-10-08 09:50:27 +0600

    breadcrumb updated

--- src/components/breadcrumb/BreadCrumb.tsx
+++ src/components/breadcrumb/BreadCrumb.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
-import {
-	Box,
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-} from '@chakra-ui/react';
+import { Box, Breadcrumb, BreadcrumbItem } from '@chakra-ui/react';
 
 import { MdKeyboardArrowRight } from 'react-icons/md';
 import Link from 'next/link';
+import { colors } from '@/lib/config/colors';
 const BreadCrumb = () => {
 	const endpoint = usePathname();
 	console.log('endpoint:', endpoint.slice());
 	return (
-		<Box ml='3rem'>
+		<Box px='24px' mb='16px' maxW='620px' mx='auto'>
 			<Breadcrumb
 				separator={<MdKeyboardArrowRight size={20} color='gray.500' />}
 			>
-				<BreadcrumbItem>
+				<BreadcrumbItem color={colors.success}>
 					<Link href='/'> Home</Link>
 				</BreadcrumbItem>
 
